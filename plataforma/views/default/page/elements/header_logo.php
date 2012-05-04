773e6fcda754e0caf61cc1be0d62e7d0640e6998
David Romaní <david@flux.cat>
2012-05-04 18:05:30 +0200

    Header somenergia logo addded

--- plataforma/views/default/page/elements/header_logo.php
+++ plataforma/views/default/page/elements/header_logo.php
@@ -10,6 +10,6 @@ $site_url = elgg_get_site_url();
 
 <h1>
 	<a class="elgg-heading-site" href="<?php echo $site_url; ?>">
-		<?php echo $site_name; ?>
+		<img src="_graphics/Logo-SomEnergia-Verd-72.jpg" style="width:90px; margin-right:20px; vertical-align: text-bottom; border-radius:7px; box-shadow:0px 0px 10px #999;" /><?php echo $site_name; ?>
 	</a>
 </h1>
