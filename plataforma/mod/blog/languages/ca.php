e4723a5285c3e02fe077175c98be5ce2c0b067e2
David Romaní <david@flux.cat>
2012-05-22 09:19:42 +0200

    i18n catalan translation blog module added

--- plataforma/mod/blog/languages/ca.php
+++ plataforma/mod/blog/languages/ca.php
@@ -1,58 +1,69 @@
 <?php
-
-// Generat per la traducció del navegador 
+/**
+ * Blog Catalan language file.
+ *
+ */
 
 $catalan = array( 
-	 'blog'  =>  "Bloc" , 
-	 'blogs'  =>  "Blocs" , 
-	 'blog:user'  =>  "Bloc de %s" , 
-	 'blog:user:friends'  =>  "Bloc d'amistats de %s" , 
-	 'blog:your'  =>  "Bloc personal" , 
-	 'blog:posttitle'  =>  "Bloc de %s: %s" , 
-	 'blog:friends'  =>  "Blocs de les teves amistats" , 
-	 'blog:yourfriends'  =>  "Blocs recents de les teves amistats" , 
-	 'blog:everyone'  =>  "Tots els blocs de la xarxa" , 
-	 'blog:read'  =>  "Llegir bloc" , 
-	 'blog:addpost'  =>  "Escriure notícia al bloc" , 
-	 'blog:editpost'  =>  "Edita el missatge del bloc" , 
-	 'blog:text'  =>  "Text del bloc" , 
-	 'blog:strapline'  =>  "%s" , 
-	 'item:object:blog'  =>  "Comentaris en el blog" , 
-	 'blog:never'  =>  "mai" , 
-	 'blog:preview'  =>  "Pre-visualitzar" , 
-	 'blog:draft:save'  =>  "Desar esborrany" , 
-	 'blog:draft:saved'  =>  "Darrer esborrany" , 
-	 'blog:comments:allow'  =>  "Acceptar comentaris" , 
-	 'blog:preview:description'  =>  "Això és una pre-visualització del seu enviament." , 
-	 'blog:preview:description:link'  =>  "Continuar editant o enregistrar el seu enviament, clica aquí." , 
-	 'blog:river:created'  =>  "%s ha escrit" , 
-	 'blog:river:updated'  =>  "%s actualitzat" , 
-	 'blog:river:posted'  =>  "%s enregistrat" , 
-	 'blog:river:create'  =>  "una nova entrada titulada:" , 
-	 'blog:river:update'  =>  "títol de la notícia" , 
-	 'blog:river:annotate'  =>  "un comentari a la notícia:" , 
-	 'blog:posted'  =>  "La notícia ha estat desada al bloc" , 
-	 'blog:deleted'  =>  "La notícia ha estat eliminada del bloc" , 
-	 'blog:save:failure'  =>  "La notícia no pot ser desada. Prova de nou o posat en contacte amb el grup administrador" , 
-	 'blog:moreblogs'  =>  "Més entrades de bloc" , 
-	 'blog:numbertodisplay'  =>  "Nombre de notícies del bloc a desplegar" , 
-	 'blog:blank'  =>  "Has d'omplir el títol i el cos del missatge per a poder enviar-lo." , 
-	 'blog:notfound'  =>  "No trobem cap enviament a aquest bloc. Prova una altra cerca." , 
-	 'blog:notdeleted'  =>  "No podem esborrar aquest enviament" , 
-	 'blog:newpost'  =>  "Nova notícia del bloc desada" , 
-	 'blog:via'  =>  "via blocs" , 
-	 'blog:enableblog'  =>  "Habilitar bloc del grup" , 
-	 'blog:group'  =>  "Blog del grup" , 
-	 'blog:error'  =>  "Alguna cosa ha anat malament, prova de nou." , 
-	 'groups:enableblog'  =>  "Habilitar blog del grup" , 
-	 'blog:nogroup'  =>  "Aquest grup no té cap post encara" , 
-	 'blog:more'  =>  "Més comentaris del bloc" , 
-	 'blog:read_more'  =>  "Llegir notícia sencera" , 
-	 'blog:widget:description'  =>  "Aquest widget mostra les seves darreres entrades al bloc
-" , 
-	 'blog:conversation'  =>  "Conversa"
+    'blog' => "Blocs",
+    'blog:blogs' => "Blocs",
+    'blog:revisions' => "Revisions",
+    'blog:archives' => "Arxiu",
+    'blog:blog' => "Bloc",
+    'item:object:blog' => "Blocs",
+
+    'blog:title:user_blogs' => "Blocs de %s",
+    'blog:title:all_blogs' => "Tots els blocs del lloc web",
+    'blog:title:friends' => "Blocs dels amics",
+
+    'blog:group' => "Bloc del grup",
+    'blog:enableblog' => "Habilitar el bloc del grup",
+    'blog:write' => "Afegir una entrada al bloc",
+
+    // Editing
+    'blog:add' => "Afegir una entrada al bloc",
+    'blog:edit' => "Editar una entrada del bloc",
+    'blog:excerpt' => "Extracte",
+    'blog:body' => "Cos",
+    'blog:save_status' => "Desat: ",
+    'blog:never' => "Mai",
+
+    // Statuses
+    'blog:status' => "Estat",
+    'blog:status:draft' => "Esborrany",
+    'blog:status:published' => "Publicat",
+    'blog:status:unsaved_draft' => "No s'ha desat l'esborrany",
+
+    'blog:revision' => "Revisió",
+    'blog:auto_saved_revision' => "La revisió s'ha desat automàticament",
+
+    // messages
+    'blog:message:saved' => "S'ha desat l'entrada al bloc.",
+    'blog:error:cannot_save' => "No s'ha pogut desar l'entrada al bloc.",
+    'blog:error:cannot_write_to_container' => "No teniu els permisos necessaris per afegir una entrada al bloc del grup.",
+    'blog:error:post_not_found' => "S'ha suprimit l'entrada, no és vàlida o no disposeu dels permisos necessaris per visualitzar-la.",
+    'blog:messages:warning:draft' => "L'entrada té un esborrany pendent de desar!",
+    'blog:edit_revision_notice' => "(Versió anterior)",
+    'blog:message:deleted_post' => "S'ha suprimit l'entrada del bloc.",
+    'blog:error:cannot_delete_post' => "No s'ha pogut suprimir l'entrada del bloc.",
+    'blog:none' => "El bloc no té entrades",
+    'blog:error:missing:title' => "Heu d'introduir el títol del bloc!",
+    'blog:error:missing:description' => "Heu d'introduir el cos del bloc!",
+    'blog:error:cannot_edit_post' => "L'entrada no existeix o no teniu permisos suficients per editar-la.",
+    'blog:error:revision_not_found' => "No s'ha trobat la revisió.",
+
+    // river
+    'river:create:object:blog' => "%s ha publicat una entrada al bloc %s",
+    'river:comment:object:blog' => "%s ha comentat al bloc %s",
+
+    // notifications
+    'blog:newpost' => "Nova entrada al bloc",
+
+    // widget
+    'blog:widget:description' => "Mostrar les últimes entrades del bloc",
+    'blog:moreblogs' => "Més entrades",
+    'blog:numbertodisplay' => "Quantitat d'entrades a mostrar",
+    'blog:noblogs' => "El bloc no té entrades"
 ); 
 
 add_translation('ca', $catalan); 
-
-?>
